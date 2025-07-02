5a1a02c71d162537914cf2b6f3b78c1f05521c8e
nouiramariem18 <nawaranouira18@gmail.com>
2025-07-02 21:02:59 +0200

    Update Greeting.tsx

--- src/Greeting.tsx
+++ src/Greeting.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-// 1️⃣ Interface pour typer les props du composant
+
 interface GreetingProps {
   name: string; // la prop "name" est une chaîne de caractères
 }
 
-// 2️⃣ Composant fonctionnel typé avec React.FC
+
 const Greeting: React.FC<GreetingProps> = ({ name }) => {
   return <div>Hello, {name}!</div>;
 };
